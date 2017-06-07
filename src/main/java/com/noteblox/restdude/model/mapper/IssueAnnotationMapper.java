ca77fa073d7765fb787e564929c98840633fa77a
manos <manos@sobol>
2017-06-07 23:57:25 +0300

    fixed headers

--- src/main/java/com/noteblox/restdude/model/mapper/IssueAnnotationMapper.java
+++ src/main/java/com/noteblox/restdude/model/mapper/IssueAnnotationMapper.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of IssueBLOX.
+ * This file is part of NoteBLOX.
  *
- * IssueBLOX is free software: you can redistribute it and/or modify
+ * NoteBLOX is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * IssueBLOX is distributed in the hope that it will be useful,
+ * NoteBLOX is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with IssueBLOX.  If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>.
+ * along with NoteBLOX.  If not, see <https://www.gnu.org/licenses/agpl-3.0.en.html>.
  */
 package com.noteblox.restdude.model.mapper;
 
